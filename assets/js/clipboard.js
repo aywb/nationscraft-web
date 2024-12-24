487e2754c9e79766f4c73476e82f24c28f9e8663
N1ghtc1 <53506942+aywb@users.noreply.github.com>
2024-12-24 19:22:38 +0200

    Add files via upload

---
+++ assets/js/clipboard.js
@@ -0,0 +1,10 @@
+function copy(){
+    navigator.clipboard.writeText('play.nationscraft.xyz')
+    swal({
+    title: 'IP COPIED',
+    text: 'Have Fun!',
+    icon: 'success',
+    timer: 2000,
+    buttons: false,
+})
+}
